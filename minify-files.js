1b568628ee296699218e49fc8ca32fdf5ec68cb9
RecipesGenerator <47058326+RecipesGenerator@users.noreply.github.com>
2022-06-27 14:50:54 +0300

    Update minify-files.js

--- minify-files.js
+++ minify-files.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 //minify("fff.html").then(a=>{fs.writeFileSync("fff.min.html",a)}).catch(console.error);
 fs.readdir('styles', (err, files) => {
   files.forEach(file => {
-    minify(file)
+    minify('./styles/' + file)
       .then(data => {
         fs.writeFileSync(`./styles/min-${file}`, data);
       })
